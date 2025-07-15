005814b056b548b099c60482fdb544f443d4e645
brianim97 <iirriartebrian@gmail.com>
2025-07-15 11:34:06 -0300

    try

--- src/helpers/backupAndReadDB.ts
+++ src/helpers/backupAndReadDB.ts
@@ -1,37 +1,36 @@
 import { execSync } from 'child_process';
-import { readFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync } from 'fs';
 import { DateTime } from 'luxon';
 import { join } from 'path';
 import { devLog } from './devLog';
-import { mkdirSync, existsSync } from 'fs';
 
 interface BackupOptions {
-    host: string;
-    user: string;
-    password: string;
-    database: string;
-    outputPath: string;
+  host: string;
+  user: string;
+  password: string;
+  database: string;
+  outputPath: string;
 }
 
 export async function backupAndReadDB({
-    host,
-    user,
-    password,
-    database,
-    outputPath,
+  host,
+  user,
+  password,
+  database,
+  outputPath,
 }: BackupOptions): Promise<string> {
-    if (!existsSync(outputPath)) {
-        mkdirSync(outputPath, { recursive: true });
-    }
+  if (!existsSync(outputPath)) {
+    mkdirSync(outputPath, { recursive: true });
+  }
 
-    const now = DateTime.now().toFormat('yyyy-LL-dd_HH-mm-ss');
-    const backupFile = join(outputPath, `${database}_${now}.sql`);
+  const now = DateTime.now().toFormat('yyyy-LL-dd_HH-mm-ss');
+  const backupFile = join(outputPath, `${database}_${now}.sql`);
 
-    const mysqldumpCommand = `mysqldump -h ${host} -u ${user} -p${password} ${database} > ${backupFile}`;
-    execSync(mysqldumpCommand);
+  const mysqldumpCommand = `mysqldump -h 127.0.0.1 -P 3306 -u ${user} -p${password} ${database} > ${backupFile}`;
+  execSync(mysqldumpCommand);
 
-    const data = readFileSync(backupFile, 'utf8');
-    devLog(`Backup created ${user}@${database}`, 'DB');
+  const data = readFileSync(backupFile, 'utf8');
+  devLog(`Backup created ${user}@${database}`, 'DB');
 
-    return data;
+  return data;
 }
