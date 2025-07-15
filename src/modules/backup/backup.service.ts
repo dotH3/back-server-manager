b61e58de34d0e7c3331807e881399bbf44347c1f
brianim97 <iirriartebrian@gmail.com>
2025-07-15 11:09:32 -0300

    fix: host

--- src/modules/backup/backup.service.ts
+++ src/modules/backup/backup.service.ts
@@ -1,30 +1,31 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { backupAndReadDB } from 'src/helpers/backupAndReadDB';
-import { BackupDbDto } from './dtos/backup-db.dto';
-import { BotService } from '../bot/bot.service';
 import { Request } from 'express';
+import { backupAndReadDB } from 'src/helpers/backupAndReadDB';
+import { BotService } from '../bot/bot.service';
+import { BackupDbDto } from './dtos/backup-db.dto';
 
 @Injectable()
 export class BackupService {
+  constructor(private botService: BotService) {}
 
-    constructor(private botService: BotService,){}
+  async postBackupNow(
+    { user, password, database, host }: BackupDbDto,
+    request: Request
+  ) {
+    try {
+      const backupContent = await backupAndReadDB({
+        host,
+        user,
+        password,
+        database,
+        outputPath: './backups',
+      });
 
-    async postBackupNow({ user, password, database }: BackupDbDto,request:Request) {
-        try {
-
-            const backupContent = await backupAndReadDB({
-                host: 'localhost',
-                user,
-                password,
-                database,
-                outputPath: './backups',
-            });
-
-            this.botService.sendMessage(`Backup creado ${database} ${request.ip}`)
-            return backupContent;
-        } catch (error) {
-            console.error(error.message);
-            throw new BadRequestException('Error creating backup');
-        }
+      this.botService.sendMessage(`Backup creado ${database} ${request.ip}`);
+      return backupContent;
+    } catch (error) {
+      console.error(error.message);
+      throw new BadRequestException('Error creating backup');
     }
+  }
 }
--- src/modules/backup/dtos/backup-db.dto.ts
+++ src/modules/backup/dtos/backup-db.dto.ts
@@ -1,28 +1,31 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class BackupDbDto {
-    @IsString()
-    @MinLength(3, {
-        message: 'User must be at least 3 characters long',
-    })
-    @MaxLength(50, {
-        message: 'User must not exceed 50 characters',
-    })
-    user: string;
+  @IsString()
+  @MinLength(3, {
+    message: 'User must be at least 3 characters long',
+  })
+  @MaxLength(50, {
+    message: 'User must not exceed 50 characters',
+  })
+  user: string;
 
-    @IsString()
-    @MinLength(6, { message: 'Password must be at least 6 characters long' })
-    @MaxLength(50, {
-        message: 'Password must not exceed 50 characters',
-    })
-    password: string;
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @MaxLength(50, {
+    message: 'Password must not exceed 50 characters',
+  })
+  password: string;
 
-    @IsString()
-    @MinLength(3, {
-        message: 'Database name must be at least 3 characters long',
-    })
-    @MaxLength(50, {
-        message: 'Database name must not exceed 50 characters',
-    })
-    database: string;
+  @IsString()
+  @MinLength(3, {
+    message: 'Database name must be at least 3 characters long',
+  })
+  @MaxLength(50, {
+    message: 'Database name must not exceed 50 characters',
+  })
+  database: string;
+
+  @IsString()
+  host: string;
 }
